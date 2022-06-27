d75f24ce145067db412c5326e02ca81567f6cf72
iamusayev <iamusayev1337@gmail.com>
2022-06-27 15:54:51 +0400

    added validation result

---
+++ src/validator/ValidationResult.java
@@ -0,0 +1,21 @@
+package validator;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationResult {
+
+    @Getter
+    private List<Error> errors = new ArrayList<>();
+
+
+    public void add(Error error) {
+        this.errors.add(error);
+    }
+
+    public boolean isValid() {
+        return errors.isEmpty();
+    }
+}
