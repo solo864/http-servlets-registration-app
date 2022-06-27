c258326ac3ef7d8f08d0a362f5ce9750ef589c88
iamusayev <iamusayev1337@gmail.com>
2022-06-27 15:54:05 +0400

    added PropertiesUtil

---
+++ src/util/PropertiesUtil.java
@@ -0,0 +1,28 @@
+package util;
+
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
+import java.util.Properties;
+
+@UtilityClass
+public class PropertiesUtil {
+
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    public static String get(String key){
+        return PROPERTIES.getProperty(key);
+    }
+
+    @SneakyThrows
+    private static void loadProperties() {
+        try(var stream = PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties")){
+            PROPERTIES.load(stream);
+        }
+    }
+
+}
